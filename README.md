5da77320d814b07d823172d1b2365cce9b7e64bf
Loïc Colat <loic.colat@gmail.com>
2020-01-21 23:31:00 +0100

    Beginning of arg catch

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # multi-thread-picture-converter
+
+#### Launch project with docker
+##### Windows
+```bash
+docker run --rm -v "%cd%":/usr/src/prog -it -w /usr/src/prog gcc:latest /bin/bash -c "gcc main.c -lpthread -o apply-effect && ./apply-effect "./in/" "./out/" <threadQuantity> <algorithm>
+```
+##### Linux
+```bash
+docker run --rm -v $(pwd):/usr/src/prog -it -w /usr/src/prog gcc:latest /bin/bash -c "gcc main.c -lpthread -o apply-effect && ./apply-effect "./in/" "./out/" <threadQuantity> <algorithm>
+```
---
+++ main.c
@@ -0,0 +1,20 @@
+//
+// Created by Loïc Colat on 21/01/2020.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+int main (int argc, char *argv[])
+{
+    char executionExample[] = "./apply-effect \"<inputFolder>\" \"<outputFolder>\" <threadQuantity> <algorithm>\n"
+                              "Example :\n"
+                              "./apply-effect \"./in/\" \"./out/\" 3 boxblur\n";
+    if(argc < 5){
+        fprintf( stderr, "Too few arguments, respect this format : \n%s", executionExample);
+    }else if(argc > 5){
+        fprintf( stderr, "Too much arguments, respect this format : \n%s", executionExample);
+    }
+    return 0;
+}
